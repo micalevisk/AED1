36ab625191e6d13512d4b500e730a71169b8a01c
Micael Levi L. Cavalcante <mykael2010@hotmail.com>
2015-10-06 19:51:54 -0400

    update indentation

--- Listas_de_Exercicios/funcoesLista1_Micael.c
+++ Listas_de_Exercicios/funcoesLista1_Micael.c
@@ -6,41 +6,41 @@
 
 // 1
 int fatorialDe(int n){
-	int fatorial=1;
+  int fatorial=1;
 	
-	while(n>0){
-		fatorial *= n;
-		n--;
-	}
+  while(n>0){
+    fatorial *= n;
+    n--;
+  }
 	
-	return fatorial;
+  return fatorial;
 }
 
 
 // 2
 void modificarVetor(int v[], int n){
-	int i=0;
+  int i=0;
 	
-	for( ; i < n; i++){
-		if(v[i]<=0) v[i] = -2;                     // a
+  for( ; i < n; i++){
+    if(v[i]<=0) v[i] = -2;                     // a
 		
-		else if(v[i] >= 1 && v[i] <= 5) v[i] = -1; // b
+    else if(v[i] >= 1 && v[i] <= 5) v[i] = -1; // b
 		
-		else v[i] = 0;                             // c
-	}
+    else v[i] = 0;                             // c
+  }
 }
 
 
 // 3
 void inverterPosicoesVetor(int v[], int n){
-	int i, j;
-	int aux;
+  int i, j;
+  int aux;
 	
-	for(i=0, j=n-1; i < j; i++, j--){
-		aux  = v[i];
-		v[i] = v[j];
-		v[j] = aux;
-	}
+  for(i=0, j=n-1; i < j; i++, j--){
+    aux  = v[i];
+    v[i] = v[j];
+    v[j] = aux;
+  }
 }
 
 
@@ -53,9 +53,9 @@ int textoVerdadeiro(char texto[]){
     if(texto[i] == 'U'){
       j=0;
       while( texto[i+1] == palavra[j] ){
-		if(j==2) return 1; // verdadeiro
-		j++;
-		i++;
+	if(j==2) return 1; // verdadeiro
+	j++;
+	i++;
       }
     }
   }
@@ -71,7 +71,7 @@ int estaEmOrdemCrescente(int v[], int n){
   if( n > 1 ){
     while( v[i] <= v[i+1] ){
       if(i == n-2) // se for o penultimo elemento
-		return 1;
+	return 1;
       i++;
     }
   }
@@ -101,77 +101,77 @@ void insertionSort(int v[], int n){
 
 // 7
 int somaDosQuadradosDe(int inteiroN){
-	int acumulador=0;
+  int acumulador=0;
 	
-	while(inteiroN > 0){
-		acumulador += inteiroN * inteiroN;
-		inteiroN--;
-	}
-	return acumulador;
+  while(inteiroN > 0){
+    acumulador += inteiroN * inteiroN;
+    inteiroN--;
+  }
+  return acumulador;
 }
 
 
 // 8
 int letraNoNome(char nome[], char letra){
-	int i;
-	int vezesQueAparece = 0;
-	char letraMinuscula, letraMaiuscula;
+  int i;
+  int vezesQueAparece = 0;
+  char letraMinuscula, letraMaiuscula;
 	
-	if((letra > 64) && (letra < 91)) letraMinuscula = letra + 32; // se for uma letra maíuscula, torna ela minúscula (ao somar com 32)
-	letraMinuscula = letraMinuscula-32;
+  if((letra > 64) && (letra < 91)) letraMinuscula = letra + 32; // se for uma letra maíuscula, torna ela minúscula (ao somar com 32)
+  letraMinuscula = letraMinuscula-32;
 	
-	for(i=0; nome[i] != '\0'; i++){
-	    if( nome[i] == letraMinuscula || nome[i] == letraMaiuscula )
-	        vezesQueAparece++;
-	}
+  for(i=0; nome[i] != '\0'; i++){
+    if( nome[i] == letraMinuscula || nome[i] == letraMaiuscula )
+      vezesQueAparece++;
+  }
 
-	return vezesQueAparece;
+  return vezesQueAparece;
 }
 
 
 // 9
 int palindromo(char nome[]){
-    int tamanho = strlen(nome);
-    int i=0, j=tamanho-1;
+  int tamanho = strlen(nome);
+  int i=0, j=tamanho-1;
 
-    for(; i < j; i++, j--){
-        if(nome[i] != nome[j]) // se não for palíndromo
-            return 0;
-    }
+  for(; i < j; i++, j--){
+    if(nome[i] != nome[j]) // se não for palíndromo
+      return 0;
+  }
 
-    return tamanho; // se tiver apenas o caracter nulo, tamanho == 0 -> falso
-                    // senão, tamanho != 0 -> verdadeiro
+  return tamanho; // se tiver apenas o caracter nulo, tamanho == 0 -> falso
+  // senão, tamanho != 0 -> verdadeiro
 }
 
 
 // 10
 int verSeRepete_binarySearch(int x, int v[], int n){
-    int i=0;
-    int inicio=0, fim = n-1, meio;
+  int i=0;
+  int inicio=0, fim = n-1, meio;
 
-    while( inicio <= fim ){
-        meio = (inicio + fim)/2;
+  while( inicio <= fim ){
+    meio = (inicio + fim)/2;
 
-        if(v[meio] < x)
-            inicio = meio + 1;
-        else if(v[meio] > x)
-            fim = meio - 1;
-        else{
-            return ((v[meio+1] != x) && (v[meio-1] != x));
-            // se for true -> a chave não se repete (considerando um vetor ordenado crescente/decrescente).
-            // onde o próximo/anterior elemento deverá ser diferente de x caso este não se repita.
-        }
+    if(v[meio] < x)
+      inicio = meio + 1;
+    else if(v[meio] > x)
+      fim = meio - 1;
+    else{
+      return ((v[meio+1] != x) && (v[meio-1] != x));
+      // se for true -> a chave não se repete (considerando um vetor ordenado crescente/decrescente).
+      // onde o próximo/anterior elemento deverá ser diferente de x caso este não se repita.
     }
+  }
 
-    return 0; // 'falso' caso a chave não exista no vetor.
+  return 0; // 'falso' caso a chave não exista no vetor.
 }
 
 
 // 11
 void substituirElementos(int v[], int n, int delta){
-    int i=0;
-	for(; i<n; i++)
-        v[i] -= delta;
+  int i=0;
+  for(; i<n; i++)
+    v[i] -= delta;
 }
 
 
@@ -201,72 +201,72 @@ void inverterSubstituicoes(int v[], int n){
 
 // 15
 int matrizQuadrada(int matriz[][N], int K){
-    int i, j;
-    int vezesQueAparece=0;
+  int i, j;
+  int vezesQueAparece=0;
 
-    for(i=0; i < N ; i++){
-        for(j=0; j < N; j++){
-            if(matriz[i][j] == K) vezesQueAparece++;
-            if (vezesQueAparece > 4) return 0; // como é maior que 4 vezes, retorna 0 (=false)
-        }
+  for(i=0; i < N ; i++){
+    for(j=0; j < N; j++){
+      if(matriz[i][j] == K) vezesQueAparece++;
+      if (vezesQueAparece > 4) return 0; // como é maior que 4 vezes, retorna 0 (=false)
     }
+  }
 
-	return (vezesQueAparece == 4); // se for exatamente 4, retorna 1 (=true)
+  return (vezesQueAparece == 4); // se for exatamente 4, retorna 1 (=true)
 }
 
 
 // 16
 int maiorElementoDaMatrizQuadrada(int matriz[][N]){
-	int i=0, j;
-	int maior = matriz[0][0];
+  int i=0, j;
+  int maior = matriz[0][0];
 	
-	for(; i < N; i++){
-		for(j=0; j < N; j++){
-		  if(matriz[i][j] > maior)
-		  	maior = matriz[i][j];
-		}
-	}
-	return maior;
+  for(; i < N; i++){
+    for(j=0; j < N; j++){
+      if(matriz[i][j] > maior)
+	maior = matriz[i][j];
+    }
+  }
+  return maior;
 }
 
 
 // 17
 int somaDiagonalSecundaria(int matriz[][N]){
-	int i, j;
-	int soma = 0;
+  int i, j;
+  int soma = 0;
 	
-	for(i=0,j=N-1; i < N; i++, j--)
-		soma += matriz[i][j];
+  for(i=0,j=N-1; i < N; i++, j--)
+    soma += matriz[i][j];
 	
-	return soma;
+  return soma;
 }
 
 
 // 18
 float desvioPadraoDe(int v[], int n[]){
-/*
-  1 - obter media geral
-  2 - subtrair cada elemento da media geral, elevar ao quadrado e soma-los
-  3 - variancia = dividir resultado anterior pelo numero de elementos
-  4 - desvio padrao = raiz quadrada da variancia.
-*/
+  /*
+    1 - obter media geral
+    2 - subtrair cada elemento da media geral, elevar ao quadrado e soma-los
+    3 - variancia = dividir resultado anterior pelo numero de elementos
+    4 - desvio padrao = raiz quadrada da variancia.
+  */
 
-	int i;
-	int somaDosQuadradosDasDifrencas=0;
-	float mediaGeral, variancia;
-	int nElementos = n; // numero de elementos.
+  int i;
+  int somaDosQuadradosDasDifrencas=0;
+  float mediaGeral, variancia;
+  int nElementos = n; // numero de elementos.
 	
-	// definindo media geral.
-	for(i=0; i<n; i++)
-		mediaGeral += (float)v[i] * (1 / (float)nElementos);
+  // definindo media geral.
+  for(i=0; i<n; i++)
+    mediaGeral += (float)v[i] * (1 / (float)nElementos);
 	
-	// subtraindo cada elemento pela mediaGeral, elevando ao quadrado e somando tudo.
-	for(i=0; i<n; i++)
-		somaDosQuadradosDasDifrencas += pow((v[i] - mediaGeral), 2) ;
+  // subtraindo cada elemento pela mediaGeral, elevando ao quadrado e somando tudo.
+  for(i=0; i<n; i++)
+    somaDosQuadradosDasDifrencas += pow((v[i] - mediaGeral), 2) ;
 	
-	// media dos quadrados das diferencas.
-	variancia = somaDosQuadradosDasDifrencas / nElementos;
+  // media dos quadrados das diferencas.
+  variancia = somaDosQuadradosDasDifrencas / nElementos;
 	
-	return sqrt(variancia); // desvio padrao
+  return sqrt(variancia); // desvio padrao
 }
 
