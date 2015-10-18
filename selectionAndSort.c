4e0f9e1666642c6d4f37f49f56d8d26306893a7f
Micael Levi L. Cavalcante <mllc@icomp.ufam.edu.br>
2015-10-18 18:53:10 -0400

    Update selectionAndSort.c

--- selectionAndSort.c
+++ selectionAndSort.c
@@ -1,76 +1,75 @@
 #include <stdio.h>
 
-// ORDENAÇÃO POR SELEÇÃO: O(n²)
+// ORDENAÇÃO POR SELEÇÃO:
+// melhor: O(n²)   | pior: O(n²)
 void selectionSort(int v[], int n){
-  int i=0,j, aux;
-  int posMenor;
-  
-  for(; i < n - 1; i++){
+  int i=0;
+  int j, posMenor, aux;
+
+  for(; i<n; i++){
     posMenor = i;
 
-    for(j=i+1; j < n; j++){
+    for(j=i+1; j < n; j++)
       if(v[j] < v[posMenor])
 	posMenor = j;
-    }
-    if( i != posMenor ){
-      aux         = v[i];
-      v[i]        = v[posMenor];
+		
+    if(i != posMenor){
+      aux 	  = v[i];
+      v[i]	  = v[posMenor];
       v[posMenor] = aux;
     }
   }
 }
 
+// BUSCA SEQUENCIAL/LINEAR:
+// melhor: O(1)    | pior: O(n)
+int linearSearch(int chave, int v[], int n){
+  int i=0;
+  v[n] = chave;
 
-
-// BUSCA SEQUENCIAL: O(n)
-int linearSearch(int v[], int n, int chave){
-  int i;
-  v[n] = chave; //sentinela
-
-  for(i=0; v[i]!=chave; i++);
-
+  for(; v[i] != chave; i++);
   if(i < n) return i;
-  else return -1;  
-}
-
-
-
-// BUSCA BINÁRIA: O(log2 n)
-int binarySearch(int v[], int n, int chave){
-  int inicio = 0, fim = n - 1;
-  int meio;
-  
-  while(inicio <= fim){
-    meio = (inicio+fim)/2;
-
-    if(v[meio] < chave)
-      inicio = meio + 1;
-    
-    else if(v[meio] > chave)
-      fim = meio - 1;
-
-    else
-      return meio;    
-  }
   return -1;
 }
 
 
+// BUSCA BINÁRIA:
+// melhor: O(1)   | pior: O(log2 n)
+int binarySearch(int chave, int v[], int n){
+  int inicio=0, fim = n-1;
+  int meio;
 
-// ORDENAÇÃO POR INSERÇÃO: O(n²)
-void insertionSort(int v[], int n){
-  int i=1, j, pivot;
+  while(inicio <= fim){
+    meio = (inicio+fim)/2;
 
-  for(; i < n; i++){
-    pivot = v[i];
-    j = i - 1;
-    while((j >= 0) && (v[j] > pivot)){
-      v[j+1] = v[j];
-      j--;
-    }
-    v[j+1] = pivot;
+    if(v[meio] < chave) inicio = meio+1;
+    
+    else if(v[meio] > chave) fim = meio -1
+    
+    else return meio;
   }
+  
+  return -1;
 }
 
 
-/* written with GNU Emacs */
+// ORDENAÇÃO POR INSERÇÃO:
+// melhor: O(n)   | pior: O(n²)
+void insertionSort(int v[], int n){
+  int i=1;
+  int j, pivot;
+
+  for(; i<n; i++){
+    pivot = v[i];
+    j = i-1;
+
+    while( (j >= 0) && (v[j] > pivot) ){
+      v[j+1] = v[j];
+      j--;
+    }
+    
+    v[j+1] = pivot;
+  }    
+}
+
+/* written with GNU Emacs editor */
