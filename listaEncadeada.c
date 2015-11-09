102f43889b5e1c48d4c4fbaa8359bfbf42da4105
Micael Levi L. Cavalcante <mllc@icomp.ufam.edu.br>
2015-11-09 12:04:47 -0400

    Update listaEncadeada.c

--- listaEncadeada.c
+++ listaEncadeada.c
@@ -1,50 +1,114 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct No{
+#include <time.h>
+
+typedef  struct tipoNo {
   int val;
-  struct No *prox;
-}tipoNo;
+  struct tipoNo *prox;
+} tipoNo;
 
 
-void criarLista(tipoNo **prim){
-  *prim = NULL;
+void criarLista(tipoNo **p ) {
+ *p = NULL;
 }
 
-void inserirNaLista(tipoNo **prim, int valor){
-  tipoNo *aux = (tipoNo*) malloc(sizeof(tipoNo));
+void inserirNaLista(tipoNo **p, int valor) {
+  tipoNo *aux;
+  aux = (tipoNo*) malloc(sizeof(tipoNo));
+  
   aux->val = valor;
-
-  aux->prox = *prim;
-  *prim     = aux;
+  aux->prox = *p;
+  *p = aux;
 }
 
-int valorDoPrimeiro(tipoNo *prim){
-  if(prim) return prim->val; // prim != NULL
-  return 0;
-}
-
-void mostrarLista(tipoNo *prim){
-
-  while(prim){
-    printf("%d, ",p->val);
-    p = p->prox;    
+void mostrarElementosLista(tipoNo *p) {
+  while(p) {
+    printf("%d ",p->val);
+    p = p->prox;
   }
   printf("\n");
 }
 
 
+int buscarElementoNaLista(tipoNo *p, int elemento){
+  while(p){
+    if(elemento == p->val) return 1; // elemento encontrado
+    p = p->prox;
+  }
+  return 0; // elemento NAO encontrado
+}
 
 
-int main(){
 
-  tipoNo *primeiro;
-  
-  criarLista(&primeiro);
-  
-  inserirNaLista(&primeiro, 1);
-  inserirNaLista(&primeiro, 2);
-  inserirNaLista(&primeiro, 3);
-  
-  mostrarLista(primeiro); // 3, 2, 1,
+int removerElemento(tipoNo *p, int elemento){
+  tipoNo *aux;
+
+  while(p){
+    aux = p->prox;
+    if(elemento == aux->val){
+      p->prox = aux->prox;
+      free(aux);
+      return 1; // elemento removido
+    }
+    p = p->prox;
+  }
+  return 0; // nao encontrou o elemento
+}
+
+
+int removerElemento2(tipoNo **prim, int elemento){
+  tipoNo *aux = NULL;
+  tipoNo *p   = *prim;
+
+  while(p){
+
+    if(elemento == p->val){
+      if(aux == NULL) *prim = p->prox;
+      else             aux->prox = p->prox;
+        free(p);
+      }      
+      return 1; // elemento removido
+    }
+    aux = p;
+    p = p->prox;
+  }
+  return 0; // nao encontrou o elemento
+}
+
+
+
+
+int main() {
+  srand(time(NULL));
+
+  tipoNo *prim;
+  criarLista(&prim);
+
+  int fim=1;
+  int numero;
+
+  for(numero=0; numero < 5; numero++) // criar lista com 5 elementos
+    inserirNaLista(&prim, rand()%12); // 0 a 11
+
+/* 
+  while(fim){
+    printf(">> Digite um numero para inserir: ");
+    scanf("%d",&numero);
+    inserirNaLista(&prim, numero);
+
+    printf(">> Deseja inserir um valor na lista? (0 = nao): ");
+    scanf("%d",&fim);
+  }
+*/
+
+  mostrarElementosLista(prim);
+  mostrarElementosLista(prim->prox->prox); // mostrar a partir do 3o elemento
+
+  printf("\n>> Qual valor deseja buscar na lista?: ");
+  scanf("%d",&numero);
+
+  (buscarElementoNaLista(prim, numero)) ? printf(">> Valor encontrado\n") : printf(">> Valor nao encontrado\n");
+
 }
