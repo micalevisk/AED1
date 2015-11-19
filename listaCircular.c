b866648f333bd324afcef8d32fdee583ce2bbf4d
Micael Levi L. Cavalcante <mykael2010@hotmail.com>
2015-11-19 19:34:12 -0400

    fixed: listaCircular.c

--- listaCircular.c
+++ listaCircular.c
@@ -3,92 +3,101 @@
 #include <string.h>
 
 typedef struct{
-	int valor;
-	unsigned quantidade;
+  int valor;
+  unsigned quantidade;
 } tipoDado;
 
 typedef struct no{
-	tipoDado dado;
-	struct no *prox;
+  tipoDado dado;
+  struct no *prox;
 } tipoNo;
 
 typedef struct{
-	tipoNo *atual;	
+  tipoNo *atual;	
 } tipoListaCircular;
 
 
 void criarListaCircular(tipoListaCircular *a){
-	a->atual = NULL;
+  a->atual = NULL;
 }
 
 
 void inserirNaListaCircular(tipoListaCircular *a, tipoDado elemento){ // insere depois do 'atual'
-	tipoNo *aux = (tipoNo*) malloc(sizeof(tipoNo));
-	tipoNo *temp;
+  tipoNo *aux = (tipoNo*) malloc(sizeof(tipoNo));
+  tipoNo *temp = a->atual;
 
-	if(aux){
-		aux->dado = elemento;
-
-		if(!a->atual) a->atual = aux;
-		temp 		   = a->atual->prox;
-		a->atual->prox = aux;
-		aux            = temp;
-	}
+  if(aux){
+    aux->dado = elemento;
+    if(!temp) a->atual = aux; // lista vazia
+    aux->prox = a->atual->prox;
+    a->atual->prox = aux;    
+  }
 }
 
 
 tipoNo* haElementosNaListaCircular(tipoListaCircular *a){
-	return a->atual;
+  return a->atual;
 }
 
 
 // [lista nunca vazia]
-tipoDado removerDaListaCircular(tipoListaCircular *a){ // remove o posterior ao 'atual' e atualiza o ponteiro
-	tipoNo *aux  = a->atual;
-	tipoNo *proximo= aux->prox;;
-	tipoDado temp = proximo->dado;
+tipoDado removerDaListaCircular(tipoListaCircular *a){ // remove o sucessor de 'atual'
+  tipoNo *aux    = a->atual;
+  tipoNo *proximo= aux->prox;;
+  tipoDado temp  = proximo->dado;
 
-	// para lista de 1 elemento
-	if(aux == proximo) a->atual = NULL;
+  // para lista de 1 elemento
+  if(aux == proximo) a->atual = NULL;
 
-	// para lista com mais de 1 elemento
-	else aux->prox = proximo->prox;
-	free(proximo);
+  // para lista com mais de 1 elemento
+  else aux->prox = proximo->prox;
+  free(proximo);
 
-	return temp;
+  return temp;
 }
 
 
 void mostrarElementosDaListaCircular(tipoListaCircular *a){
-	tipoNo *aux = a->atual;
-
-	while(aux){
-		printf(" valor: %d | quantidade: %u \n", aux->dado.valor, aux->dado.quantidade);
-		aux = aux->prox;
-	}
+  tipoNo *aux = a->atual;
+  tipoNo *temp= aux;
+  
+  if(aux){
+    do{
+      printf(" valor: %d | quantidade: %u \n", aux->dado.valor, aux->dado.quantidade);
+      aux = aux->prox;
+    }while(aux != temp);
+  }
 }
 
 
 int main(){
 
-	tipoListaCircular lista;
-	tipoDado d1, d2, d3;
-	d1.valor = 1;     d2.valor = 1;       d3.valor = 3;
-	d1.quantidade = 2; d2.quantidade = 3; d3.quantidade = 4;
+  tipoListaCircular lista;
+  tipoDado d1, d2, d3;
+  d1.valor = 1;      d2.valor = 2;      d3.valor = 3;
+  d1.quantidade = 1; d2.quantidade = 2; d3.quantidade = 3;
 
-	criarListaCircular(&lista);
+  criarListaCircular(&lista);
 
-	printf("\n>> Ha elementos? ");
-	(haElementosNaListaCircular(&lista)) ? printf(" SIM\n") : printf(" NAO\n");
+  printf("\n>> Ha elementos? ");
+  (haElementosNaListaCircular(&lista)) ? printf(" SIM\n") : printf(" NAO\n");
 
-	inserirNaListaCircular(&lista, d1); // atual
-	inserirNaListaCircular(&lista, d2);
-	inserirNaListaCircular(&lista, d3);
+  printf("\n>> Inserindo elemtos: \n");
+  inserirNaListaCircular(&lista, d1); // atual
+  inserirNaListaCircular(&lista, d2);
+  inserirNaListaCircular(&lista, d3);
+  inserirNaListaCircular(&lista, d3);
 	
-	printf("\n>> Mostrando elementos: \n");
-	mostrarElementosDaListaCircular(&lista);
-	/*
-	>> Removendo o proximo de atual: \n");
-	removerDaListaCircular(&lista);
-	*/
+  printf("\n>> Mostrando elementos: \n");
+  mostrarElementosDaListaCircular(&lista);
+  
+  int loop=0;
+  while(loop>=0){
+    scanf("%d", &loop);
+    printf("\n>> Removendo o proximo de atual e mostrando: \n");
+    if(haElementosNaListaCircular(&lista)) removerDaListaCircular(&lista);
+    else printf("\n <vazia> \n");
+    mostrarElementosDaListaCircular(&lista);
+  }
+  
 }
